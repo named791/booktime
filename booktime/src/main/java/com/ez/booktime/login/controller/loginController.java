09b9ee6fb1dcfaade328ff1b8738a927a06a819b
bsg <bsg@DESKTOP-0G5QCD8>
2020-01-05 16:21:22 +0900

    index.jsp의 변경
    카테고리 바 추가
    메인 사진 링크 수정
    
    2020-01-05 4:19 백슬기

---
+++ booktime/src/main/java/com/ez/booktime/login/controller/loginController.java
@@ -0,0 +1,34 @@
+package com.ez.booktime.login.controller;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class loginController {
+	private static final Logger logger 
+		= LoggerFactory.getLogger(loginController.class);
+	
+	@RequestMapping("/login/login.do")
+	public String login() {
+		logger.info("로그인 화면");
+		
+		return "login/login";
+	}
+	
+	@RequestMapping("/mypage/memberInfo.do")
+	public String memberInfo() {
+		logger.info("회원정보");
+		
+		return "mypage/memberInfo";
+	}
+	
+	@RequestMapping("/freeBoard/List.do")
+	public String freeBoardList() {
+		logger.info("자유게시판 목록");
+		
+		return "freeBoard/List";
+	}
+}
