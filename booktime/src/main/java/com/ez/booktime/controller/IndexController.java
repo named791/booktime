2ac3f09123f20500d1584c14f16eed650ed8ecd2
bsg0909 <bsg0909@daum.net>
2020-01-14 17:34:54 +0900

    -카테고리 리스트 불러오기 기능 구현 com.ez.booktime.controller의 Category.java -자주 물어보는 질문 창 bottom의 버튼 클릭 시 이동
    <2020-01-14 오후 5:34 백슬기>

--- booktime/src/main/java/com/ez/booktime/controller/IndexController.java
+++ booktime/src/main/java/com/ez/booktime/controller/IndexController.java
@@ -1,20 +1,56 @@
 package com.ez.booktime.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ez.booktime.api.AladinAPI;
 
 @Controller
 public class IndexController {
 	private static final Logger logger
 		=LoggerFactory.getLogger(IndexController.class);
 	
+	
+	
 	@RequestMapping("/index.do")
 	public String index() {
 		logger.info("index 화면 보여주기");
 		
 		return "index";
+	}
+	
+	@RequestMapping("/category.do")
+	public String categoryList(@RequestParam(defaultValue="0")
+	int cateNo, Model model) throws Exception {
+		logger.info("카테고리 번호={}",cateNo);
+		
+		Category category=new Category();
+		List<Map<String, Object>> list=category.categoryFind(cateNo);
+		logger.info("카테고리 검색 리스트 크기={}",list.size());
+		
+		model.addAttribute("list",list);
+		
+		return "category";
+	}
+	
+	@RequestMapping("/search.do")
+	public String showSearchResult(@RequestParam(defaultValue="0")
+	String searchKeyword, Model model) throws Exception{
+		logger.info("검색어 searchKeyword={}",searchKeyword);
+
+		return "searchList";
+	}
+	
+	@RequestMapping("/faq.do")
+	public void showFAQ() {
+		logger.info("자주 묻는 질문");
 	}
 }
