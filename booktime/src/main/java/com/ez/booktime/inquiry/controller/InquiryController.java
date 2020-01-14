6f936f1ff7747675f0fcc1a57c5573223264c553
com <com@DESKTOP-M7665GE>
2020-01-14 15:45:35 +0900

    01-14 1차 => 1:1 문의 이메일 발송 성공 시 메시지 추가, 발송 성공 후 메인으로 이동

--- booktime/src/main/java/com/ez/booktime/inquiry/controller/InquiryController.java
+++ booktime/src/main/java/com/ez/booktime/inquiry/controller/InquiryController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,7 +29,7 @@ public class InquiryController {
 	}
 	
 	@RequestMapping(value="/mail/mailSending.do", method = RequestMethod.POST)
-	public String mailSending(HttpServletRequest request) {
+	public String mailSending(HttpServletRequest request, Model model) {
 		String tomail  = "yjwyhg@gmail.com";     // 받는 사람 이메일
 		String setfrom = request.getParameter("email");         
 		String title   = request.getParameter("title");      // 제목
@@ -37,16 +38,17 @@ public class InquiryController {
 		String userid = request.getParameter("userid");
 		String msg = request.getParameter("message");
 		
-	    String content = "\r\n문의 유형 : "+ type + 
-	    				"\r\n" + "\r\n"
-	    				+ "아이디 : "+ userid + 
-	    				"\r\n" + "\r\n"
-	    				+ "이름 : "+name+
-	    				"\r\n" + "\r\n"
-	    				+ "이메일 : "+ setfrom + 
-	    				"\r\n" + "\r\n"
-	    				+ "내용 : "+ msg;// 내용
+	    String content ="<strong>문의 유형</strong> : "+ type + 
+	    				"<br>" + "<br>"
+	    				+ "<strong>아이디</strong> : "+ userid + 
+	    				"<br>" + "<br>"
+	    				+ "<strong>이름</strong> : "+name+
+	    				"<br>" + "<br>"
+	    				+ "<strong>이메일</strong> : "+ setfrom + 
+	    				"<br>" + "<br>"
+	    				+ "<strong>내용</strong> : "+ msg;// 내용
 	   
+	    String alert="", url="/mail/mailSending.do";
 	    setfrom = setfrom.toLowerCase();
 	    tomail = tomail.toLowerCase();
 	    
@@ -58,16 +60,20 @@ public class InquiryController {
 	      messageHelper.setTo(tomail);     // 받는사람 이메일
 	      messageHelper.setFrom(setfrom);  // 보내는사람 생략하거나 하면 정상작동을 안함
 	      messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
-	      messageHelper.setText(type);
-	      messageHelper.setText(name);
-	      messageHelper.setText(userid);
-	      messageHelper.setText(content);  // 메일 내용
+
+	      messageHelper.setText(content,true);  // 메일 내용
 	     
 	      mailSender.send(message);
+	      alert="문의 메일을 발송하였습니다.";
+	      url="/index.do";
 	    } catch(Exception e){
 	      System.out.println(e);
+	      alert="메일 발송에 실패하였습니다.";
 	    }
 	   
-	    return "redirect:/inquiry/inquiryPage.do";
+	    model.addAttribute("msg", alert);
+	    model.addAttribute("url", url);
+	    
+	    return "common/message";
 	  }
 }
