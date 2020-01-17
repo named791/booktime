611d1a76d0a3a7ec16de7f9bd22a8fde08226ec9
yjwyhg <yjwyhg@naver.com>
2020-01-17 18:24:07 +0900

    01/17 2차 - 상품 목록 화면 수정 중 / 이미지 파일 추가

--- booktime/src/main/java/com/ez/booktime/book/controller/BooklistController.java
+++ booktime/src/main/java/com/ez/booktime/book/controller/BooklistController.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ez.booktime.common.SearchVO;
 import com.ez.booktime.controller.Category;
 
 @Controller
@@ -17,9 +19,24 @@ public class BooklistController {
 	private static final Logger logger
 		= LoggerFactory.getLogger(BooklistController.class);
 
+	/*
+	 * @RequestMapping("/book/bookList.do") public String
+	 * bookList_post(@RequestParam(defaultValue="0") int cateNo, Model model) throws
+	 * Exception { logger.info("카테고리 번호 cateNo={}",cateNo);
+	 * 
+	 * Category category=new Category(); List<Map<String, Object>>
+	 * list=category.categoryFind(cateNo);
+	 * logger.info("카테고리 검색 리스트 크기={}",list.size());
+	 * 
+	 * model.addAttribute("list",list);
+	 * 
+	 * return "book/bookList"; }
+	 */
+	
 	@RequestMapping("/book/bookList.do")
-	public String bookList_post(@RequestParam(defaultValue="0")
-	int cateNo, Model model) throws Exception {
+	public String bookList_post(@ModelAttribute SearchVO searchVo,
+			@RequestParam(defaultValue="0") int cateNo, 
+			@RequestParam String title, Model model) throws Exception {
 		logger.info("카테고리 번호 cateNo={}",cateNo);
 		
 		Category category=new Category();
